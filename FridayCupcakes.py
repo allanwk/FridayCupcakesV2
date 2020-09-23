49c14ce4629cc248be790d660d2d8c4f3c5d0fdd
allan <allankretzmann@hotmail.com>
2020-09-23 20:58:23 -0300

    Implementado módulo generate_bills.py
    O módulo realiza análise dos pedidos; gera as mensagens
    de confirmação, salvando um arquivo txt; e faz estimativas
    de gasto e lucro

--- FridayCupcakes.py
+++ FridayCupcakes.py
@@ -4,11 +4,33 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pandas as pd
+import datetime
+from generate_bills import generate_bills
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/spreadsheets']
-
 ORDERS_SHEET_ID = '1tDdoexzEMvBmHiFFPs9nl_UO92kfa7amWEgoyeeLWCI'
+
+recipes = {
+    'maracujá': {
+        'farinha': 14.595, 'açúcar': 17.764, 'óleo':11.008, 'ovos': 14.595, 'fermento': 0.625,
+        'chocolate branco': 29.19, 'creme de leite': 1.901, 'maracujá': 0.083
+        },
+    'limão': {
+        'farinha': 14.595, 'açúcar': 14.595, 'óleo':11.008, 'ovos': 14.595, 'fermento': 0.625,
+        'leite condensado': 7.005, 'creme de leite': 3.544, 'limão': 0.145, 'margarina': 4.587,
+        'açúcar de confeiteiro': 16.68
+        },
+    'churros': {
+        'farinha': 14.595, 'açúcar': 14.595, 'óleo':11.008, 'ovos': 14.595, 'fermento': 0.625,
+        'canela': 0.2, 'leite condensado lata': 32.916
+        },
+    'brigadeiro': {
+        'farinha': 14.595, 'açúcar': 17.764, 'óleo':11.008, 'ovos': 14.595, 'fermento': 0.625,
+        'chantilly': 8.333, 'chocolate em pó': 1.25, 'leite condensado': 16.471, 'creme de leite': 8.34,
+        'chocolate meio amargo': 2.502, 'chocolate ao leite': 1.668, 'margarina': 0.667
+        }
+}
 
 def main():
     creds = None
@@ -48,16 +70,22 @@ def main():
     stock = stock_result.get('values', [])
 
     #Converting data obtained to dataframes
-    orders_df = pd.DataFrame()
     labels = ['Cliente', 'Maracujá', 'Limão', 'Churros', 'Brigadeiro', 'PedirEndereço']
+    orders_df = pd.DataFrame(columns=labels)
     for i in range(len(labels)):
-        orders_df[labels[i]] = pd.Series(orders[i])
-    orders_df = orders_df.fillna(0)
-
+        if i != 0 and i != 5:
+            orders_df[labels[i]] = pd.Series(orders[i], dtype=int)
+        else:
+            orders_df[labels[i]] = pd.Series(orders[i], dtype=str)
+    orders_df = orders_df.fillna(int(0))
+    
     stock_df = pd.DataFrame()
     stock_df['Ingrediente'] = pd.Series(stock[0], dtype=str)
     stock_df['Qty'] = pd.Series(stock[1], dtype=int)
     stock_df = stock_df.fillna(0)
+
+    #Analisando pedidos
+    print(generate_bills(orders_df))
 
 if __name__ == '__main__':
     main()
---
+++ generate_bills.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import datetime
+
+def generate_bills(df):
+    """Generates a text file containing the order confirmations (bills) of each costumer,
+    and also calculates finnancial metrics, returning those in the form a dictonary.
+    """
+
+    total = 0
+    cost = 0
+    numCupcakes = 0
+
+    flavors = ["Maracujá", "Limão", "Churros", "Brigadeiro"]
+    info = pd.DataFrame({'flavor': ["Maracujá", "Limão", "Churros", "Brigadeiro"],
+                        'perCupcake': [1.24, 0.61, 0.63, 0.83]})
+    info = info.set_index('flavor')
+
+    order_confirmations = open("orders.txt", "w+", encoding='UTF-8')
+    order_confirmations.write(("\n▬▬▬▬▬▬▬▬▬▬▬▬▬" + str(datetime.date.today()) + "▬▬▬▬▬▬▬▬▬▬▬▬▬\n"))
+
+    for index, row in df.iterrows():
+        order_confirmations.write("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n")
+        sum = int(row["Maracujá"]) + int(row["Limão"]) + int(row["Churros"]) + int(row["Brigadeiro"])
+        price = sum*2.5
+        if (sum > 5):
+            price*=0.95
+        elif (sum == 5):
+            price = 12.00
+        total += price
+        numCupcakes += sum
+
+        order_confirmations.write("\n" + row["Cliente"] + "\n")
+        order_confirmations.write("Oie, o dia das entregas está chegando!\n\n")
+        order_confirmations.write("Vamos conferir seu pedido:\n")
+        for flavor in flavors:
+            cost += row[flavor] * info.at[flavor, "perCupcake"]
+            if row[flavor] == 1:
+                order_confirmations.write("1 cupcake de " + flavor + " - R$2.50\n")
+            elif row[flavor] > 1:
+                order_confirmations.write(str(row[flavor]) + " cupcakes de " + flavor + " - R$" + str(round(2.5*row[flavor], 2)) + "\n")
+        if sum >= 5:
+            order_confirmations.write("\nTotal com desconto aplicado: R$" + str(round(price,2)) + "\n")
+        else:
+            order_confirmations.write("\nTotal: R$" + str(round(price,2)) + "\n")
+        order_confirmations.write("(apenas em dinheiro ou transferência)\n\nVocê vai estar em casa a partir das 18h00 amanhã para nós entregarmos?\n")
+        if row["PedirEndereço"] == "S":
+            order_confirmations.write("Além disso, em que endereço você deseja que entreguemos?\n")
+
+        order_confirmations.close()
+
+        profit = total - cost
+        return {
+            'Cost': round(cost, 2), 
+            'Income': round(total, 2),
+            'Profit': round(profit, 2),
+            'CupcakesSold': numCupcakes,
+            'ProfitPerCupcake': round(profit/numCupcakes, 2)
+        }
